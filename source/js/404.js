ce70c1b67e6faf29087173efda32577893e2420a
Evgeni Genchev <120590298+EvgeniGenchev07@users.noreply.github.com>
2025-07-05 20:08:52 +0300

    commit

--- source/js/404.js
+++ source/js/404.js
@@ -1,10 +1,1 @@
-const events = ['resize', 'load'];
-const visual = document.getElementById("visual");
-events.forEach(function(e){
-    window.addEventListener(e, function(){
-        const width = window.innerWidth;
-        const height = window.innerHeight;
-        const ratio = 45 / (width / height);
-      visual.style.transform = "translate(-50%, -50%) rotate(-" + ratio + "deg)";
-    });
-});
+const events=["resize","load"],visual=document.getElementById("visual");events.forEach((function(n){window.addEventListener(n,(function(){const n=45/(window.innerWidth/window.innerHeight);visual.style.transform="translate(-50%, -50%) rotate(-"+n+"deg)"}))}));
